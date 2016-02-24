bbb0a1dafa3a2360e0ea27b4ada6ff457c9f65bd
Christopher Schmitt <me@chris.yt>
2016-02-24 22:33:25 +0100

    Initial commit

---
+++ api.js
@@ -0,0 +1,63 @@
+var _ = require('lodash');
+var async = require('async');
+module.exports = function (config) {
+
+    var ovh = require('ovh')({
+        endpoint: config.api.endpoint,
+        appKey: config.api.key,
+        appSecret: config.api.secret,
+        consumerKey: config.api.consumerKey
+    });
+
+    var listZones = function listZones(callback) {
+        ovh.request('GET', '/domain/zone/', function (err, zones) {
+            callback(err, zones);
+        })
+    };
+
+    var getRecord = function getRecord(zone, id, callback) {
+        ovh.request('GET', '/domain/zone/' + zone + '/record/' + id, function (err, record) {
+            callback(err, record);
+        });
+    };
+
+    var listRecords = function listRecords(zone, callback) {
+        listZones(function (err, zones) {
+            if (!_.includes(zones, zone)) {
+                return callback('Zone "' + zone + '" not found.', null);
+            }
+            ovh.request('GET', '/domain/zone/' + zone + '/record/', function (err, recordIds) {
+                async.map(recordIds, function (recordId, cb) {
+                    getRecord(zone, recordId, cb);
+                }, callback);
+            })
+        });
+    };
+
+    var createRecord = function listRecords(zone, subDomain, fieldType, target, callback) {
+        // For validation
+        listZones(function (err, zones) {
+
+            if (!_.includes(zones, zone)) {
+                return callback('Zone "' + zone + '" not found.', null);
+            }
+            if (!_.includes(['A', 'AAAA', 'CNAME', 'MX', 'TXT'], fieldType)) {
+                return callback('Invalid FieldType, only "A","AAAA","CNAME","MX" and "TXT" are valid.', null)
+            }
+            ovh.request('POST', '/domain/zone/' + zone + '/record/', {
+                fieldType: fieldType,
+                subDomain: subDomain,
+                target: target,
+                ttl: 0
+            }, callback);
+        });
+    };
+
+    return {
+        listZones: listZones,
+        getRecord: getRecord,
+        listRecords: listRecords,
+        createRecord: createRecord
+    }
+
+};
---
+++ helper.js
@@ -0,0 +1,45 @@
+var _ = require('lodash');
+var validator = require('validator');
+module.exports = {
+    inputValidateAndSetConfig: function (program) {
+
+        // Whoo this is my Lucky day, there are Credentials inside the ENV lets use them ^, yay!)
+        if (process.env.OVH_DNS_APP_KEY) {
+            program.applicationKey = process.env.OVH_APP_KEY;
+        }
+
+        if (process.env.OVH_DNS_APP_SECRET) {
+            program.applicationSecret = process.env.OVH_APP_SECRET;
+        }
+
+        if (process.env.OVH_DNS_CONSUMER_KEY) {
+            program.consumerKey = process.env.OVH_CONSUMER_KEY;
+        }
+
+        if (!program.applicationKey || !validator.isAlphanumeric(program.applicationKey)) {
+            console.error("Application Key is missing or not a valid String [Client Side Invalid]");
+            process.exit(1);
+        }
+        if (!program.applicationSecret || !validator.isAlphanumeric(program.applicationSecret)) {
+            console.error("Application Secret is missing or not a valid String [Client Side Invalid]");
+            process.exit(1);
+        }
+        if (!program.consumerKey || !validator.isAlphanumeric(program.consumerKey)) {
+            console.error("Consumer Key is missing or not a valid String [Client Side Invalid]");
+            process.exit(1);
+        }
+        if (!program.endpoint || !_.includes(['ovh-eu', 'ovh-ca'], program.endpoint)) {
+            console.error("Endpoint is invalid, needs to be 'ovh-eu' or 'ovh-ca' [Client Side Invalid]");
+            process.exit(1);
+        }
+
+        return {
+            api: {
+                key: program.applicationKey,
+                secret: program.applicationSecret,
+                endpoint: program.endpoint,
+                consumerKey: program.consumerKey
+            }
+        }
+    }
+};
